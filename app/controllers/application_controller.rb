9defed2efc8a73dff4239fdf87bd337d79659f67
edwin <ngisahedwin@gmail.com>
2022-11-10 00:26:04 +0300

    added routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,65 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/veterinarians' do
+    vets = Veterinarian.all.order(:id)
+    vets.to_json
   end
+
+  get '/veterinarians/:id' do
+    vet = Veterinarian.find(params[:id])
+    vet.to_json
+  end
+
+  get '/patients' do
+    pets = Patient.all.order(:id)
+    pets.to_json
+  end
+
+  get '/patients/:id' do
+    patient = Patient.find(params[:id])
+    patient.to_json
+  end
+
+  delete '/patients/:id' do
+    patient = Patient.find(params[:id])
+    patient.destroy
+    patient.to_json
+  end
+
+  post '/patients' do
+    patient = Patient.create(
+      name: params[:name],
+      breed: params[:breed],
+      age: params[:age],
+      weight: params[:weight],
+      owner_id: params[owner_id],
+      animal_type: params[animal_type],
+      sex: params[sex]
+    )
+    patient.to_json
+  end
+
+  get '/owners' do
+    owners = Owner.all.order(:id)
+    owners.to_json
+  end
+
+  get '/owners/:id' do
+    owner = Owner.find(params[:id])
+    owner.to_json
+  end
+
+  get '/appointments' do
+    appts = Appointment.all.order(:id)
+    appts.to_json
+  end
+
+  delete '/appointments/:id' do
+    appointment = Appointment.find(params[:id])
+    appointment.destroy
+    appointment.to_json
+  end
+
 
 end
